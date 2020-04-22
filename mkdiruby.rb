674967d365bd2b4a746773be78b3a46cf0c8e7a2
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-22 12:00:24 +0200

    Update : SECOND Task Automation : app,views, db, app.rb

--- mkdiruby.rb
+++ mkdiruby.rb
@@ -41,14 +41,35 @@ def create_lib(name_folder)
   Dir.mkdir("#{name_folder}/lib")
 end
 
+def create_app_and_views_in_lib(name_folder)
+  Dir.mkdir("#{name_folder}/lib/app")
+  Dir.mkdir("#{name_folder}/lib/views")
+end
+
+def create_base_in_views(name_folder)
+  file = File.open("#{name_folder}/lib/views/done.rb", "a")
+  file.close
+  file = File.open("#{name_folder}/lib/views/index.rb", "a")
+  file.close
+end
+
+def create_db(name_folder)
+  Dir.mkdir("#{name_folder}/db")
+  file = File.open("#{name_folder}/db/thing.csv", "a")
+  file.close
+end
+
 def create_app_rb(name_folder)
   file = File.open("#{name_folder}/app.rb", "a")
   file.puts("require 'bundler'")
   file.puts("Bundler.require")
-  file.puts("#HERE LINKS")
-  file.puts("require_relative 'lib/...'")
-  file.puts("require_relative 'lib/...'")
-  file.puts("binding.pry")
+  file.puts(" ")
+  file.puts('$:.unshift File.expand_path("./../lib", __FILE__)')
+  file.puts("#require '...' <- fichier/class")
+  file.puts(" ")
+  file.puts("#MyClass.new.perform <- Appel de class")
+  file.puts(" ")
+  file.puts("#binding.pry")
   file.close
 end
 
@@ -59,7 +80,7 @@ def initiate_rspec(name_folder)
 end
 
 def create_readme(name_folder)
-  file = File.open("#{name_folder}/readme.md", "a")
+  file = File.open("#{name_folder}/README.md", "a")
   file.puts("# TASK AUTOMATION")
   file.puts("Generate with Ruby")
   file.close
@@ -72,6 +93,9 @@ def perform
   create_env(get_argv)
   create_gitignore_with_env(get_argv)
   create_lib(get_argv)
+  create_app_and_views_in_lib(get_argv)
+  create_base_in_views(get_argv)
+  create_db(get_argv)
   create_app_rb(get_argv)
   initiate_rspec(get_argv)
   create_readme(get_argv)
